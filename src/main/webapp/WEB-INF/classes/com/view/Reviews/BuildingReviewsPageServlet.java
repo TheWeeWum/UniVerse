937a1e449b04c9317900d9be3572b95982f1b5e0
raonkimm <raon.kim@mail.utoronto.ca>
2023-12-02 17:40:13 -0500

    My buildings review page servlet accidentally got deleted in the xml file, so i got it back. Also renamed it so it's clearer which servlet it is

--- src/main/webapp/WEB-INF/classes/com/view/Reviews/BuildingReviewsPageServlet.java
+++ src/main/webapp/WEB-INF/classes/com/view/Reviews/BuildingReviewsPageServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-public class ReviewsPageServlet extends HttpServlet {
+public class BuildingReviewsPageServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String buildingCode = request.getParameter("buildingCode");
